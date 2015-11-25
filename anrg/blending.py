eb2d664d50b826aabaf9aa8c92732ffca5269dbd
zaxliu <zaxliu>
2015-11-25 10:11:30 +0800

    Third 20151124.

--- anrg/blending.py
+++ anrg/blending.py
@@ -1,14 +1,74 @@
+import numbers
 import numpy as np
+import scipy.sparse as sp
 from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.linear_model import LinearRegression
-from sklearn.cross_validation import check_cv, _safe_split
+from sklearn.cross_validation import _safe_split, KFold, StratifiedKFold, ShuffleSplit
+from sklearn.utils.multiclass import type_of_target
+from sklearn.utils.validation import _num_samples
 
+
+def check_cv(cv, X=None, y=None, classifier=False):
+    """Input checker utility for building a CV in a user friendly way.
+    Parameters
+    ----------
+    cv : int, double, cross-validation generator or an iterable, optional
+        Determines the cross-validation splitting strategy.
+        Possible inputs for cv are:
+        - None, to use the default 3-fold cross-validation,
+        - integer, to specify the number of folds.
+        - double, to specify fraction of test set
+        - An object to be used as a cross-validation generator.
+        - An iterable yielding train/test splits.
+        For integer/None inputs, if ``y`` is binary or multiclass,
+        :class:`StratifiedKFold` used. If the estimator is a classifier
+        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
+        Refer :ref:`User Guide <cross_validation>` for the various
+        cross-validation strategies that can be used here.
+    X : array-like
+        The data the cross-val object will be applied on.
+    y : array-like
+        The target variable for a supervised learning problem.
+    classifier : boolean optional
+        Whether the task is a classification task, in which case
+        stratified KFold will be used.
+    Returns
+    -------
+    checked_cv: a cross-validation generator instance.
+        The return value is guaranteed to be a cv generator instance, whatever
+        the input type.
+    """
+    is_sparse = sp.issparse(X)
+    if cv is None:
+        cv = 3
+    if isinstance(cv, numbers.Integral):
+        if classifier:
+            if type_of_target(y) in ['binary', 'multiclass']:
+                cv = StratifiedKFold(y, cv)
+            else:
+                cv = KFold(_num_samples(y), cv)
+        else:
+            if not is_sparse:
+                n_samples = len(X)
+            else:
+                n_samples = X.shape[0]
+            cv = KFold(n_samples, cv)
+    elif isinstance(cv, float):
+        if classifier:
+            raise NotImplementedError
+        else:
+            if not is_sparse:
+                n_samples = len(X)
+            else:
+                n_samples = X.shape[0]
+            cv = ShuffleSplit(n=n_samples, test_size=cv, random_state=12345)
+    return cv
 
 class BlendedRegressor(BaseEstimator, RegressorMixin):
     """Blending multiple regressors into a meta-regressor
 
     Blending the output of multiple regressors into a meta-regressor using a blending model. User can
-    specify how tranning data is splitted between base-training and blending-training phase.
+    specify how training data is splitted between base-training and blending-training phase.
 
     Parameters
     ----------
@@ -16,7 +76,7 @@ class BlendedRegressor(BaseEstimator, RegressorMixin):
         Tuple of base model objects.
     blending_model : object, default: LinearRegression()
         The model used for blending base models
-    blending_split : None, int, or object, default: None
+    blending_split : None, int, double (0 to 1) or object, default: None
         The splitting method used for base- and blending training
 
     Attributes
